8d759a4fc586bdd01cbe010451a85636c32075b8
Micalgenus <micalgenus@gmail.com>
2019-03-21 17:59:54 +0900

    Update movie search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,32 @@
 class SearchController < ApplicationController
+  PAGE_COUNT = 20
+  PAGINATION = 3
+
   def movie
-    @books = "abc"
+    @page = params[:page].to_i > 0 ? params[:page].to_i : 1
+    @no = (@page - 1) * PAGE_COUNT
+
+    @movies = Movie
+      .where(
+        "title like :query or original_title like :query",
+        { :query => "%#{params[:query]}%" }
+      )
+      .left_outer_joins(movie_cast: :person)
+      .group(:id)
+      .limit(PAGE_COUNT).offset((@page - 1) * PAGE_COUNT)
+
+    count = Movie.where(
+      "title like :query or original_title like :query",
+      { :query => "%#{params[:query]}%" }
+    )
+    .left_outer_joins(movie_cast: :person)
+    .group(:id).length
+      
+    @last = [1, (count.to_f / PAGE_COUNT).ceil].max
+
+    @start = [1, @page - PAGINATION].max
+    @end = [@last, @page + PAGINATION].min
+
   end
   
   def tv
