72198ece7c8fe78b250a42ae02bae416c86004cd
Micalgenus <micalgenus@gmail.com>
2019-03-21 22:40:37 +0900

    Update rails configuration for production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Encoding::default_internal = Encoding::UTF_8
+  Encoding::default_external = Encoding::UTF_8
 end
