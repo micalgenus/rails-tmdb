590eaefd87f8f2f5616164d53601b3fafc2dd25a
Micalgenus <micalgenus@gmail.com>
2019-03-21 23:39:51 +0900

    Update model

--- db/migrate/20190320000327_add_detail_to_tv_episode_casts.rb
+++ db/migrate/20190320000327_add_detail_to_tv_episode_casts.rb
@@ -1,6 +1,6 @@
 class AddDetailToTvEpisodeCasts < ActiveRecord::Migration[5.2]
   def change
-    add_column :tv_episode_casts, :character, :string
+    add_column :tv_episode_casts, :character, :text
     add_column :tv_episode_casts, :order, :decimal
   end
 end
--- db/migrate/20190320000453_add_detail_to_movie_casts.rb
+++ db/migrate/20190320000453_add_detail_to_movie_casts.rb
@@ -1,6 +1,6 @@
 class AddDetailToMovieCasts < ActiveRecord::Migration[5.2]
   def change
-    add_column :movie_casts, :character, :string
+    add_column :movie_casts, :character, :text
     add_column :movie_casts, :order, :decimal
   end
 end
